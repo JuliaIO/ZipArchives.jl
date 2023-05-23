dfe03e873a3b76704be6b7d3b49f1823e2ed4317
nhz2 <nhz2@cornell.edu>
2023-05-23 11:48:56 -0400

    work

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using Test
 using Random
+using ZipArchives
 
 Random.seed!(1234)
 
+
+
+@test Any[] == detect_ambiguities(Base, Core, ZipArchives)
 include("test_simple-usage.jl")
+include("test_writer.jl")
+
--- test/test_simple-usage.jl
+++ test/test_simple-usage.jl
@@ -2,3 +2,37 @@ using ZipArchives
 using Test
 
 
+
+
+@testset "Write zip file" begin
+    filename = tempname()
+    # Open a new zip file with `ZipWriter`
+    # If a file already exists at filename, it will be replaced.
+    # Using the do syntax ensures the file will be closed.
+    # Otherwise make sure to close the ZipWriter to finish writing the file.
+    ZipWriter(filename) do w
+        # Write data to "test1.txt" inside the zip archive.
+        # `ZipArchives.newfile` turns w into an IO that represents a file in the archive.
+        zip_newfile(w, "test1.txt")
+        write(w, "I am data inside test1.txt in the zip file")
+
+        # Write an empty file.
+        # After calling `newfile` there is no direct way to edit any previous files in the archive.
+        zip_newfile(w, "empty.txt")
+
+        # Write data to "test2.txt" inside the zip file.
+        zip_newfile(w, "test2.txt")
+        write(w, "I am data inside test2.txt in the zip file")
+    end
+
+    # Read a zip file with `ZipReader`
+    ZipReader(filename) do r
+        @test r.names == ["test1.txt", "empty.txt", "test2.txt"]
+        zip_openentry(r, 1)
+
+
+    end
+
+
+
+end
