c3acd77a19dd6125834abcb39a8f0340fb8bae28
nhz2 <nhz2@cornell.edu>
2023-06-12 16:04:14 -0400

    test big zip files

---
+++ test/test_big_zips.jl
@@ -0,0 +1,58 @@
+using ZipArchives
+using Test
+
+@testset "$N many entries" for N in [0, 1, 2^16-1, 2^16,]
+    filename = tempname()
+    ZipWriter(filename) do w
+        for i in 1:N
+            zip_writefile(w,"$i",codeunits("$(-i)"))
+        end
+    end
+    ZipFileReader(filename) do r
+        @test zip_nentries(r) == N
+        for i in 1:N
+            @test zip_name(r, i) == "$(i)"
+            zip_openentry(r, i) do file
+                @test read(file, String) == "$(-i)"
+            end
+        end
+    end
+    rm(filename)
+end
+
+@testset "large uncompressed size" begin
+    filename = tempname()
+    ZipWriter(filename) do w
+        zip_newfile(w, "bigfile";
+            compression_method = ZipArchives.Deflate,
+            compression_level = 1,
+        )
+        x = zeros(UInt8,2^17)
+        for i in 1:2^16
+            write(w, x)
+        end
+    end
+    ZipFileReader(filename) do r
+        @test zip_nentries(r) == 1
+        zip_test_entry(r, 1)
+    end
+    rm(filename)
+end
+
+@testset "large offsets" begin
+    filename = tempname()
+    ZipWriter(filename) do w
+        x = rand(UInt8,2^17)
+        for i in 1:2^16
+            zip_writefile(w,"$i", x)
+        end
+    end
+    ZipFileReader(filename) do r
+        @test zip_nentries(r) == 2^16
+        for i in 1:2^16
+            @test zip_name(r, i) == "$(i)"
+            zip_test_entry(r, i)
+        end
+    end
+    rm(filename)
+end
