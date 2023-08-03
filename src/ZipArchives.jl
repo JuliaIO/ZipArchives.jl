021dc25696b7b1b5ed0040f9e34dc27e99196e5a
Nathan Zimmerberg <39104088+nhz2@users.noreply.github.com>
2023-08-03 09:56:58 -0400

    Update module docstring (#27)
    * update docstring
    
    * shorten docstring

--- src/ZipArchives.jl
+++ src/ZipArchives.jl
@@ -1,3 +1,48 @@
+"""
+### ZipArchives
+
+### Reading Zip archives
+
+Archives can be read from any `AbstractVector{UInt8}` containing the data of a zip archive.
+
+For example if you download this repo as a ".zip" from github https://github.com/JuliaIO/ZipArchives.jl/archive/refs/heads/main.zip 
+you can read the README in julia.
+
+```julia
+using ZipArchives
+import Downloads
+data = take!(Downloads.download("https://github.com/JuliaIO/ZipArchives.jl/archive/refs/heads/main.zip", IOBuffer()));
+archive = ZipBufferReader(data)
+```
+
+```julia
+zip_names(archive)
+```
+
+```julia
+zip_readentry(archive, "ZipArchives.jl-main/README.md", String) |> print
+```
+
+### Writing Zip archives
+
+```julia
+using ZipArchives, Test
+filename = tempname()
+```
+
+```julia
+ZipWriter(filename) do w
+    @test_throws ArgumentError zip_newfile(w, "test\\test1.txt")
+    zip_newfile(w, "test/test1.txt")
+    write(w, "I am data inside test1.txt in the zip file")
+
+    zip_newfile(w, "test/empty.txt")
+
+    zip_newfile(w, "test/test2.txt")
+    write(w, "I am data inside test2.txt in the zip file")
+end
+```
+"""
 module ZipArchives
 
 using PrecompileTools
