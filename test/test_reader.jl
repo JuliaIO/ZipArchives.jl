522b9d180aa5e31771edb4bf12e9bdbee8dde217
Nathan Zimmerberg <39104088+nhz2@users.noreply.github.com>
2024-07-09 22:40:27 -0400

    Experimental `zip_entry_data_offset` and `zip_compression_method` functions. (#63)
    * add zip_compression_method and zip_entry_data_offset helpers
    
    * add docstring
    
    * Add tests for getting offset with unknown method
    
    * Bump version to 2.1.4

--- test/test_reader.jl
+++ test/test_reader.jl
@@ -147,10 +147,16 @@ end
     data_b64 = "UEsDBD8AAgAOAHJb0FaLksVmIgAAABAAAAAJAAAAbHptYV9kYXRhCQQFAF0AAIAAADoaCWd+rnMR0beE5IbQKkMGbV//6/YgAFBLAQI/AD8AAgAOAHJb0FaLksVmIgAAABAAAAAJAAAAAAAAAAAAAACAAQAAAABsem1hX2RhdGFQSwUGAAAAAAEAAQA3AAAASQAAAAAA"
     data = base64decode(data_b64)
     r = ZipReader(data)
-    @test_throws ArgumentError("invalid compression method: 14. Only Store and Deflate supported for now") zip_test_entry(r, 1)
-    @test_throws ArgumentError("invalid compression method: 14. Only Store and Deflate supported for now") zip_openentry(r, 1)
+    @test_throws ArgumentError("invalid compression method: 14. Only Store(0) and Deflate(8) supported for now") zip_test_entry(r, 1)
+    @test_throws ArgumentError("invalid compression method: 14. Only Store(0) and Deflate(8) supported for now") zip_openentry(r, 1)
     @test zip_iscompressed(r, 1)
     @test zip_names(r) == ["lzma_data"]
+    @test ZipArchives.zip_compression_method(r, 1) === 0x000e
+    entry_data_offset = 39
+    compressed_size = 34
+    @test ZipArchives.zip_entry_data_offset(r, 1) === Int64(entry_data_offset)
+    @test ZipArchives.zip_entry_data_offset(r, big(1)) === Int64(entry_data_offset)
+    @test ZipArchives.zip_compressed_size(r, 1) === UInt64(compressed_size)
 end
 
 @testset "reading file with zip64 disk number" begin
