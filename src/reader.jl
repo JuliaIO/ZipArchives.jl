8a9beee4aa024610a52f92313cbd0725519def50
nhz2 <nhz2@cornell.edu>
2024-09-01 00:54:05 -0400

    fix docs typo

--- src/reader.jl
+++ src/reader.jl
@@ -252,7 +252,7 @@ function zip_test_entry(r::ZipReader, i::Integer)::Nothing
 end
 
 """
-    zip_test(x::ZipReader)::Nothing
+    zip_test(r::ZipReader)::Nothing
 
 Test all entries in the archive in order from `1` to `zip_nentries(r)`
 Throw an error for the first invalid entry.
